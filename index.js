25841027af494379ba6fea70bb240534c3bdd2c7
Lionel Bertrand <lionel.bertrand.prestataire@sfr.com>
2016-12-06 14:49:43 +0100

    add socket

--- index.js
+++ index.js
@@ -1,13 +1,65 @@
-// note, io(<port>) will create a http server for you
-var io = require('socket.io')(2700);
+// const config = require('./config/config.json');
+//
+// var PeerServer = require('peer').PeerServer;
+// var server = PeerServer({port: 2700, key: config.hash});
+//
+// let Peer = require('peerjs');
+//
+// server.on('connection', function(id) {
+//   console.log('connecting', id);
+//   server.send('helloWorld')
+// });
+//
+// server.on('disconnect', function(id) { console.log('disconnect'); });
+//
+//
+// server.on('data', function(data){
+//   console.log(data);
+// })
 
-io.on('connection', function (socket) {
-  socket.on('testConnection', function (cb) {
-    console.log('connect ok');
-    cb();
-  });
+var http = require("http").createServer(),
+    io = require("socket.io").listen(http);
+//
+http.listen(2700, function(){
+  io.on('connection', function (socket) {
+    console.log('connection');
 
-  socket.on('disconnect', function () {
-    io.emit('user disconnected');
+    socket.emit('etablish');
+    //
+    // socket.on('testHash', function (data, cb) {
+    //   console.log(data);
+    //   if(config.hash === data.hash){
+    //     cb(true);
+    //   }
+    //   else{
+    //     cb(false);
+    //   }
+    // });
+    //
+    // socket.on('disconnect', function () {
+    //   io.emit('user disconnected');
+    // });
   });
 });
+
+// var io = require('socket.io').listen(2700);
+//
+// io.on('connection', function (socket) {
+//   console.log('connection');
+//
+//   socket.emit('etablish');
+//
+//   socket.on('testHash', function (data, cb) {
+//     console.log(data);
+//     if(config.hash === data.hash){
+//       cb(true);
+//     }
+//     else{
+//       cb(false);
+//     }
+//   });
+//
+//   socket.on('disconnect', function () {
+//     io.emit('user disconnected');
+//   });
+// });
